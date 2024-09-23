ffb46938358653872cbf39e8c06764063b3f1ed3
0xamitr <0xamitr@gmail.com>
2024-09-23 22:14:32 +0530

    adf

--- functions/createroom.js
+++ functions/createroom.js
@@ -5,11 +5,7 @@ import { setGame } from './gamemanager.js'
 export default function createRoom(e, setSocket, name, id) {
     e.preventDefault()
     const code = Math.floor(Math.random() * 1000);
-    let socket = io("http://143.110.251.223:3005", {
-        forceNew: true,
-        origins: 'https://chess-self-two.vercel.app/',
-        transports: ['websocket'],
-    });
+    let socket = io("https://143.110.251.223:3005")
     console.log(socket)
     console.log(id)
     console.log("Socket:", socket);
--- functions/joinroom.js
+++ functions/joinroom.js
@@ -5,11 +5,7 @@ import { setGame } from './gamemanager.js'
 
 export default function joinRoom(e, setSocket, name, id) {
     e.preventDefault();
-    let socket = io("http://143.110.251.223:3005", {
-        forceNew: true,
-        origins: 'https://chess-self-two.vercel.app/',
-        transports: ['websocket'],
-    });
+    let socket = io("https://143.110.251.223:3005")
     console.log(socket);
     const val = Number(e.target.num.value);
     socket.emit('submit', val, id, name);
