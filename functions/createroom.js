53e10fcf6afc96c120e052dfdce07ffc9312fe38
0xamitr <0xamitr@gmail.com>
2024-09-25 13:00:47 +0530

    prod

--- functions/createroom.js
+++ functions/createroom.js
@@ -5,7 +5,7 @@ import { setGame } from './gamemanager.js'
 export default function createRoom(e, setSocket, name, id) {
     e.preventDefault()
     const code = Math.floor(Math.random() * 1000);
-    let socket = io("https://143.110.251.223:3005/")
+    let socket = io("https://socket.chessy.tech:3005/")
     console.log(socket)
     console.log(id)
     console.log("Socket:", socket);
