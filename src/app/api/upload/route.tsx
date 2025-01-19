24a9cdc888bb99cf725d7dcb54ddfb696c68bcd9
0xamitr <0xamitr@gmail.com>
2025-01-19 22:31:02 +0530

    i am back

--- src/app/api/upload/route.tsx
+++ src/app/api/upload/route.tsx
@@ -24,12 +24,13 @@ export async function POST(req: Request) {
     const winner = await User.findById(body.winner.id);
     const loser = await User.findById(body.players.filter((player: any) => player.id !== body.winner.id)[0].id);
 
+    console.log(game._id);
     console.log("winner", winner);
     if (winner) {
         await User.findByIdAndUpdate(body.winner.id, { games: [...winner.games, { _id: game._id, winner: winner._id, creation: game.creation, moves: game.moves }] })
     }
     if (loser) {
-        await User.findByIdAndUpdate(body.players.filter((player: any) => player.id !== body.winner.id)[0].id, { games: [...loser.games, { _id: game._id, winner: winner._id, creation: game.creation, moves: game.moves }] })
+        await User.findByIdAndUpdate(body.players.filter((player: any) => player.id !== body.winner.id)[0].id, { games: [...loser.games, { _id: game._id, winner: winner ? winner._id: null, creation: game.creation, moves: game.moves }] })
     }
 
     return NextResponse.json({ success: false }, { status: 200 });
